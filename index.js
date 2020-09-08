e6aa059a2172dc4f48e90349e16a8ba0e28613bd
caojiayuan <cjy632258@hotmail.com>
2020-09-08 20:29:21 +0800

    udpate

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ try {
   const input = core.getInput('input');
   const output = core.getInput('output');
 
-
+  const src = path.resolve(__dirname, input)
+  const dest = path.resolve(__dirname, output)
+  console.log(`generte apidoc input:${src} output:${desc}`)
   const doc = createDoc({
-    src: path.resolve(__dirname, input),
-    dest: path.resolve(__dirname, output)
+    src: src,
+    dest: dest
   })
-  
+
   if (typeof doc !== 'boolean') {
     // Documentation was generated!
     console.log(doc.data) // `api_data.json` file content
     console.log(doc.project) // `api_project.json` file content
   }
 } catch (error) {
+  console.log(`generte apidoc error`)
   core.setFailed(error.message);
 }
